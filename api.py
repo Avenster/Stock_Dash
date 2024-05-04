852cdde6890453576f1d26b6abd9f0b056ec2551
Dyuti <dyuti.dasmahapatra.21cse@bmu.edu.in>
2024-05-04 17:02:22 +0530

    api

--- api.py
+++ api.py
@@ -14,15 +14,38 @@ def get_price():
     soup = BeautifulSoup(response.text, 'html.parser')
     class_name = "YMlKec fxKbKc"  # Class name for the price
     price = float(soup.find(class_=class_name).text.strip()[0:].replace(",", ""))
-    print(price)
     return price
+
+def get_other_data():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    class_name = "YMlKec"  # Class name for the price
+    prices = soup.find_all(class_=class_name)
+
+    class_perct = "JwB6zf V7hZne"
+    value = soup.find_all(class_=class_perct)
+
+    unique_prices = set()
+    unique_perct = set()
+
+    for i in range(8):
+        if i < len(prices):
+            price = prices[i].text.strip()
+            unique_prices.add(price)
+        if i < len(value) and i < 4:
+            perct = value[i].text.strip()
+            unique_perct.add(perct)
+
+    return list(unique_prices), list(unique_perct)
 
 @app.route('/price')
 def price():
     price = get_price()
-    return jsonify({'price': price})
+    unique_prices, unique_perct = get_other_data()
+    data = {'price': price, 'unique_prices': unique_prices, 'unique_perct': unique_perct}
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True,port=3010)
+    app.run(debug=True, threaded=True, port=3020)
     while True:
         time.sleep(10)
