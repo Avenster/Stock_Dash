4a62e2b46c4e4deda1e7464f25252aed9c6ec87e
Dyuti <dyuti.dasmahapatra.21cse@bmu.edu.in>
2024-05-13 23:54:41 +0530

    My name is Dyuti

---
+++ API/plot1.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.manifold import MDS
+from sklearn.cluster import KMeans
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import json
+
+closing_prices_df = pd.read_csv('./recent.csv',index_col='Date')
+stock_data_df = pd.read_csv('./Datasets/stock_data.csv',index_col='Date')
+
+combined_df = pd.concat([stock_data_df, closing_prices_df], axis=0, sort=False)
+
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+# Extract correlation data
+correlation_matrices_json = data['correlation']
+
+# Convert correlation data from dictionary to DataFrame
+correlation_matrices = [pd.DataFrame(cm) for cm in correlation_matrices_json]
+
+numeric_combined_df = combined_df.select_dtypes(include='number')
+
+last_subset = numeric_combined_df.iloc[-20:]
+last_correlation_matrix = last_subset.corr()
+
+correlation_matrices.append(last_correlation_matrix)
+
+with open('predicted_price.json', 'r') as file:
+    data = json.load(file)
+
+data['last_correlation_matrix'] = last_correlation_matrix.to_dict()
+
+# Save the updated data back to the JSON file
+with open('predicted_price.json', 'w') as file:
+    json.dump(data, file, indent=4)
+
+def calculate_similarity_matrix(matrix1, matrix2):
+    absolute_difference = np.abs(matrix1 - matrix2)
+    similarity = absolute_difference.mean().mean()
+    return similarity
+
+num_matrices = len(correlation_matrices)
+epochs_per_year = 25
+
+similarity_matrix = np.zeros((num_matrices, num_matrices))
+
+for i in range(num_matrices):
+    for j in range(num_matrices):
+        similarity_matrix[i, j] = calculate_similarity_matrix(correlation_matrices[i], correlation_matrices[j])
+
+
+mds = MDS(n_components=3, dissimilarity='precomputed', random_state=42)
+mds_coordinates = mds.fit_transform(similarity_matrix)
+
+# Step 2: Predict the cluster for the last similarity
+last_similarity_coordinates = mds_coordinates[-1].reshape(1, -1)  # Reshape for compatibility with clustering
+kmeans = KMeans(n_clusters=4)  # Define the number of clusters
+kmeans.fit(mds_coordinates[:-1])  # Fit KMeans on all but the last similarity
+cluster_labels = kmeans.fit_predict(mds_coordinates)
+
+similarities_list = similarity_matrix.tolist()
+mds_result_list = mds_coordinates.tolist()
+cluster_labels_list = cluster_labels.tolist()
+
+
+
+
+predicted_cluster = kmeans.predict(last_similarity_coordinates)
+
+print("Predicted Cluster for the Last Similarity:", predicted_cluster[0]+1)
+
+predictions = int(predicted_cluster[0])+1
+
+last_similarity_coordinates_list = last_similarity_coordinates.tolist()
+
+# Update data dictionary with lists instead of arrays
+data = {
+    'predictions': predictions,
+    'last_similarity_coordinates': last_similarity_coordinates_list,
+    'similarities': similarities_list,
+    'mds_result': mds_result_list,
+    'cluster_labels': cluster_labels_list
+}
+
+# Write data to JSON file
+with open('data1.json', 'w') as f:
+    json.dump(data, f)
+
+
+
+
+
---
+++ API/prediction.py
@@ -0,0 +1,44 @@
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+import json
+
+model = load_model('Models/model.h5')
+new_data = pd.read_csv('./Datasets/Updated_sentiment.csv')
+
+def make_predictions(new_data, model, time_step=30):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    new_cp_data = new_data['cp'].values.reshape(-1, 1)
+    new_sentiment_data = new_data['Sentiment_Score'].values.reshape(-1, 1)
+    scaled_new_cp_data = scaler.fit_transform(new_cp_data)
+    new_df = np.concatenate([scaled_new_cp_data, new_sentiment_data], axis=1)
+    
+    new_X = []
+    for i in range(len(new_df) - time_step):
+        a = new_df[i:(i+time_step), :]
+        new_X.append(a)
+    new_X = np.array(new_X)
+
+    predictions = model.predict(new_X)
+    scaled_predictions = scaler.inverse_transform(predictions)
+
+    y_true = new_cp_data[time_step:]
+    y_pred = scaled_predictions
+
+    # Print the last predicted price
+    last_predicted_price = y_pred[-1][0]
+    return last_predicted_price
+
+last_predicted_price = make_predictions(new_data, model)
+print(last_predicted_price)
+
+# Convert float32 to Python float
+last_predicted_price = float(last_predicted_price)
+
+# Save predicted price as JSON
+predicted_price_dict = {'last_predicted_price': last_predicted_price}
+with open('predicted_price.json', 'w') as json_file:
+    json.dump(predicted_price_dict, json_file)
+
+print("Predicted price saved successfully as 'predicted_price.json'")
